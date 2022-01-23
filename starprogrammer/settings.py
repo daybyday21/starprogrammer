55d3b201a4adbd029b54c666752c1bad0f399988
yonggill-home <yonggill@wishket.com>
2022-01-24 01:55:19 +0900

    allowed host

--- starprogrammer/settings.py
+++ starprogrammer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%jtctfdyxp@#&(@_-4=jpim7+qes!d%=q@4oo(wqr9_ay7-^w!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["starprogrammer.net", "starprogrammer.kr"]
 
 
 # Application definition
